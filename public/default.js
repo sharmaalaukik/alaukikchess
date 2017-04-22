7f245228dea505f1a20106211afbfdd468cf4e27
Alaukik Sharma <you@nobody.com>
2017-04-22 20:18:50 +0530

    intial commit

---
+++ public/default.js
@@ -0,0 +1,35 @@
+var board;
+var game;
+// setup my socket client
+ var socket = io();
+
+
+
+window.onload = function () {
+    initGame();
+};
+
+var initGame = function() {
+   var cfg = {
+       draggable: true,
+       position: 'start',
+       onDrop: handleMove,
+   };
+
+   board = new ChessBoard('gameBoard', cfg);
+   game = new Chess();
+};
+
+// called when a player makes a move on the board UI
+var handleMove = function(source, target) {
+    var move = game.move({from: source, to: target});
+
+    if (move === null)  return 'snapback';
+    else socket.emit('move', move);
+
+};
+// called when the server calls socket.broadcast('move')
+socket.on('move', function (msg) {
+    game.move(msg);
+    board.position(game.fen()); // fen is the board layout
+});
